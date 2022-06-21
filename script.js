11bd3b01d4854a98fe83e342ea3f0ff5203757f2
ExQRFx <exqr.fx@gmail.com>
2022-06-21 22:47:56 +0500

    need to fix 'X' and 'O' interchanging each other

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const Game = () => {
+    let round = 1;
+
+    const getRound = () => round;
+    const setRound = () => round++;
+
+    const createGame = () => {
+        const gameBoard = GameBoard();
+        gameBoard.createBoard();
+        gameBoard.renderBoard();
+        return gameBoard;
+    }
+    
+    return {getRound, setRound, createGame}
+}
+
+
+
 const GameBoard = () => {
     const squares = [];
 
@@ -17,7 +35,16 @@ const GameBoard = () => {
             document.getElementById('board').appendChild(renderedSquare);
         }
     }
-    return {getSquares, createBoard, renderBoard}
+
+    const getStates = (squares) => {
+        let statesArr = [];
+        for (let i=0; i<=8; i++) {
+            statesArr.push(squares[i].getState());
+        }
+        return statesArr;
+    }
+
+    return {getStates, getSquares, createBoard, renderBoard}
 }
 
 const Square = () => {
@@ -29,14 +56,31 @@ const Square = () => {
     const setState = (newState) => state = newState;
 
     const createSquare = (id) => {
-        let square = document.createElement('div');
+        let square = document.createElement('button');
+        square.setAttribute('type', 'button');
         square.setAttribute('number', id);
         square.setAttribute('class', 'square');
+        square.addEventListener('click', (e) => {
+            if (game.getRound() === 0) {
+                setState('X');
+                game.setRound();
+            } else if (game.getRound() % 2 === 0) {
+                setState('O');
+                game.setRound();
+            } else {
+                setState('X');
+                game.setRound();
+            }
+            square.innerHTML = state;
+            let states = gameboard.getStates(gameboard.getSquares())
+            console.log(states);
+        })
+
         return square;
     }
     return {getState, setState, createSquare};
 }
 
-const gameBoard = GameBoard();
-gameBoard.createBoard();
-gameBoard.renderBoard();
+
+const game = Game();
+const gameboard = game.createGame();
