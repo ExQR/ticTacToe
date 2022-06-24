b6493da4085a34314405b6dea4771019d7240d27
ExQRFx <exqr.fx@gmail.com>
2022-06-24 21:11:12 +0500

    final

--- script.js
+++ script.js
@@ -11,7 +11,41 @@ const Game = () => {
         return gameBoard;
     }
     
-    return {getRound, setRound, createGame}
+    const checkWinner = (gameBoard) => {
+        let states = gameBoard.getStates(gameBoard.getSquares());
+        const winCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [6, 4, 2]
+        ];
+
+        for (comb of winCombinations) {
+            if (states[comb[0]] && states[comb[1]] && states[comb[2]]) {
+                if (states[comb[0]] === states[comb[1]] && states[comb[0]] === states[comb[2]]) {
+                    return states[comb[0]];
+                }
+            }
+        }
+        if (!states.includes(null)) {
+            return 'Tie';
+        }
+    }
+
+    const renderWinner = (winner) => {
+        const congrats = document.getElementById('congrats');
+        if (winner === 'X' || winner === 'O'){
+            congrats.innerHTML = `The winner is ${winner}` 
+        } else {
+            congrats.innerHTML = 'It\'s a tie'
+        }
+    }
+
+    return {renderWinner, checkWinner, getRound, setRound, createGame}
 }
 
 
@@ -61,22 +95,32 @@ const Square = () => {
         square.setAttribute('number', id);
         square.setAttribute('class', 'square');
         square.addEventListener('click', (e) => {
-            if (game.getRound() === 0) {
+            if (game.getRound() === 1 && state === null) {
                 setState('X');
                 game.setRound();
-            } else if (game.getRound() % 2 === 0) {
+            } else if (game.getRound() % 2 === 0 && state === null) {
                 setState('O');
                 game.setRound();
-            } else {
+                if (game.checkWinner(gameboard) !== undefined) {
+                    game.renderWinner(game.checkWinner(gameboard));
+                    disableSquares();
+                };
+            } else if (state === null) {
                 setState('X');
                 game.setRound();
+                if (game.checkWinner(gameboard) !== undefined) {
+                    game.renderWinner(game.checkWinner(gameboard));
+                    disableSquares();
+                };
             }
             square.innerHTML = state;
-            let states = gameboard.getStates(gameboard.getSquares())
-            console.log(states);
         })
 
         return square;
+    }
+    const disableSquares = () => {
+        let buttons = document.querySelectorAll('.square');
+        buttons.forEach((element) => element.setAttribute('disabled', 'true'));
     }
     return {getState, setState, createSquare};
 }
@@ -84,3 +128,11 @@ const Square = () => {
 
 const game = Game();
 const gameboard = game.createGame();
+
+const resetGame = document.createElement('button');
+resetGame.setAttribute('type', 'button');
+resetGame.setAttribute('id', 'resetBtn');
+const container = document.getElementById('container');
+container.appendChild(resetGame);
+resetGame.innerHTML = 'Restart';
+resetGame.addEventListener('click', () => document.location.reload());
